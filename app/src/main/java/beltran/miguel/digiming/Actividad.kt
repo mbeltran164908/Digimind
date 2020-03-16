2ab882e93d7bf30df00b540b8d82abe4628c43eb
MiguelS <mbeltran164908@alumno.itson.edu.mx>
2020-03-15 17:40:15 -0700

    Ya funciona el gridview

---
+++ app/src/main/java/beltran/miguel/digiming/Actividad.kt
@@ -0,0 +1,3 @@
+package beltran.miguel.digiming
+
+data class Actividad(val titulo:String,val desc:String,val hora:String)
--- app/src/main/java/beltran/miguel/digiming/ui/home/HomeFragment.kt
+++ app/src/main/java/beltran/miguel/digiming/ui/home/HomeFragment.kt
@@ -1,18 +1,27 @@
 package beltran.miguel.digiming.ui.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import beltran.miguel.digiming.Actividad
 import beltran.miguel.digiming.R
+import kotlinx.android.synthetic.main.elemento_view.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    var actividades=ArrayList<Actividad>()
+    var adaptadorActividad:AdaptadorActividad?=null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,8 +30,51 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        cargarActividades()
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
+        adaptadorActividad=AdaptadorActividad(root.context,actividades)
+        root.findViewById<GridView>(R.id.gridViewActividades).adapter=adaptadorActividad
+
         return root
+    }
+
+    fun cargarActividades(){
+        for(i in 1..9){
+            actividades.add(Actividad("Practice","Everyday","17:00"))
+        }
+    }
+
+    class AdaptadorActividad: BaseAdapter {
+        var actividades=ArrayList<Actividad>()
+        var context: Context?=null
+
+        constructor(contexto: Context, actividades:ArrayList<Actividad>){
+            this.context=contexto
+            this.actividades=actividades
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var actividad=actividades[position]
+            var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as  LayoutInflater
+            var vista=inflator.inflate(R.layout.elemento_view,null)
+            vista.tx_titulo.setText(actividad.titulo)
+            vista.tx_desc.setText(actividad.desc)
+            vista.tx_hora.setText(actividad.hora)
+
+            return vista
+        }
+
+        override fun getItem(position: Int): Any {
+            return actividades[position];
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong();
+        }
+
+        override fun getCount(): Int {
+            return actividades.size
+        }
     }
 }
